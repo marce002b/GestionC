2402835291048ac545cd83d0dfb7c3cc14439fd2
marce002b <marce002b@users.noreply.github.com>
2022-07-04 13:37:00 -0300

    Add files via upload

---
+++ GestionC/Usuarios.sql
@@ -0,0 +1,19 @@
+﻿USE [ChallengeGC]
+GO
+
+/****** Object: Table [dbo].[Usuario] Script Date: 2022-07-04 1:36:26 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Usuario] (
+    [Id]       INT           NOT NULL,
+    [Nombre]   NVARCHAR (50) NULL,
+    [Apellido] NVARCHAR (50) NULL,
+    [Email]    NVARCHAR (50) NULL,
+    [Password] NCHAR (10)    NULL
+);
+
+
