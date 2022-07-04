af145125495636ef2012021add8a4226445958bc
marce002b <marce002b@users.noreply.github.com>
2022-07-04 13:41:22 -0300

    Add files via upload

---
+++ GestionC/dbo.Usuario.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Usuario] ([Id], [Nombre], [Apellido], [Email], [Password]) VALUES (1, N'Marcelo', N'Jury', N'cmjury@gmail.com', N'1111      ')
+INSERT INTO [dbo].[Usuario] ([Id], [Nombre], [Apellido], [Email], [Password]) VALUES (2, N'Carlos', N'Solano', N'marce002_2005@hotmail.com', N'2222      ')
+INSERT INTO [dbo].[Usuario] ([Id], [Nombre], [Apellido], [Email], [Password]) VALUES (3, N'Elisa', N'Santaro', N'stringsantoro@hotmail.con', N'string    ')
